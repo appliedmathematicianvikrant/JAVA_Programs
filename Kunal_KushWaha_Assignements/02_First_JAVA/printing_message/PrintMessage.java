31cea38b1124f63cf97a0695b80db18d006366c4
appliedmathematicianvikrant <106754145+appliedmathematicianvikrant@users.noreply.github.com>
2023-10-14 16:25:16 +0530

    program

---
+++ Kunal_KushWaha_Assignements/02_First_JAVA/printing_message/PrintMessage.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrintMessage {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = in.nextLine();
+
+        System.out.println("Hello "+ name+ " How are you?");
+    }
+}
