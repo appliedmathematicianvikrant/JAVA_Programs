568991cc8f67dc20377f7b7c63dab41f1ff6fa52
appliedmathematicianvikrant <106754145+appliedmathematicianvikrant@users.noreply.github.com>
2023-12-04 18:31:42 +0530

    commit

---
+++ LinearSearch/LinearSearchmatrix/BinarySearch/Ceiling/Floor/Floor.java
@@ -0,0 +1,27 @@
+
+
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6};
+        int target = 4;
+        int resultIndex = FloorBinary(arr, target);
+        System.out.println(arr[resultIndex]);
+    }
+    public static int FloorBinary(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target == arr[mid]){
+                return arr[mid];
+            }else if (target < arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid]){
+                start = mid+1;
+            }
+           
+        }
+         return end;
+    }
+}
